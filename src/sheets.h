5977a66da33ec6e76c35464568eafd7da593a679
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-12-16 19:41:46 +0530

    refactored

---
+++ src/sheets.h
@@ -0,0 +1,84 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define SIZE 50
+typedef struct sheetobject
+{
+    long long rowsCount;
+    long long colsCount;
+    char *headers[SIZE]; // col headers
+    char *grid[SIZE][SIZE]; // rows that would contains cells (columns)
+} SheetObject;
+
+
+void freeSheet(SheetObject sheet) {
+    free(*sheet.headers);
+    free(**sheet.grid);
+}
+
+// this MUTATES THE CHAR* DATA 
+SheetObject parseSheet(char *data) {
+
+    SheetObject sheet = {0};
+
+    // parsing the headers? to get the on of columns ig
+    char firstline[SIZE];
+    sscanf(data, "%50[^\n]%*c", firstline); // reading the first line
+
+    char *colHeader = strtok(firstline, ",");
+
+    do {
+        sheet.headers[sheet.colsCount] = malloc(strlen(colHeader) + 1);
+        strcpy(sheet.headers[sheet.colsCount++], colHeader);
+    }
+    while((colHeader = strtok(NULL, ",")));
+
+    // parsing the rest of the rows
+
+    char *line;
+    int lineCount = 0;
+
+    line = strtok(data, "\n"); 
+
+    // lines[lineCount] = malloc(strlen(line) + 1); // we dont want the first line its the header !
+    // strcpy(lines[lineCount++], line);
+
+    // reading the rest of the lines.
+    while((line = strtok(0, "\n"))) {
+
+        // looping over line to find the separate cells 
+        char *cells[SIZE];
+
+        char buffer[SIZE]; // this is how long a cell can be.
+        int bufferlen = 0;
+        int cellsFound = 0;
+
+        for (int charCount = 0; charCount < strlen(line); charCount++)
+        {
+            char letter = line[charCount];
+            if (letter == ',' || charCount == strlen(line) - 1) // coz [1,2,3] elms = 3 ,(s) = 2
+            {
+                // add to list of cells
+                cells[cellsFound] = malloc(bufferlen + 1); // for the null character
+                strcpy(cells[cellsFound++], buffer);
+                // reset buffer
+                bufferlen = 0;
+                memset(buffer, 0, bufferlen);
+            } else {
+                buffer[bufferlen++] = letter;
+            }
+        }
+
+        assert(cellsFound == sheet.colsCount);
+        // can we add the cell to sheet struct directly ??? ig we can lol
+        memcpy(sheet.grid[lineCount], cells, cellsFound * sizeof(cells[0]));
+        lineCount++;
+    }
+
+    sheet.rowsCount = lineCount;
+
+    return sheet;
+}
